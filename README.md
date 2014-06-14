d48dff3a2d09547f6db43bf22bb4803e724bf601
bin liu <liubin0329@gmail.com>
2014-06-14 14:18:04 +0800

    init commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 github-source-view-plugin
 =========================
 
-show space/tab/enter key with visible characters like desktop editor 
+show space/tab/enter key with visible characters like desktop editor
+
+TODOs:
+1. let user custom show characters and color.
+
---
+++ content_script.js
@@ -0,0 +1,17 @@
+﻿function showSpaces(){
+  var a = $('div.code-body')[0];
+
+  $(a).find("div.line").each(function( index ) {
+    if ($(this).html() != '<br>'){
+      $(this).append("<span style='color:#f90;'>↙</span>");
+    }
+  });
+
+  var b = $(a).html();
+  b = b.replace(/&nbsp;/g,"<span style='color:#f90;'>.</span>");
+  b = b.replace(/> </g,"><span style='color:#f90;'>.</span><");
+  b = b.replace(/\t/g,"<span style='color:#f90;'>﹏﹏﹏﹏</span>");
+  $(a).html(b);
+}
+
+showSpaces();
